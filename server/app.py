bcc09447347ee0951020d9426b859a243b507820
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2022-02-24 05:30:21 +0800

    change endpoint

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     
-    @app.route('/')
+    @app.route('/api/hello')
     def index():
         return 'hi travelling ninja'
     
